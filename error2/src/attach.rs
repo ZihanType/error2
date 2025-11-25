1834ba995196fc5159fdd44554bf878889077e48
ZihanType <ZihanType@protonmail.com>
2025-11-25 23:53:06 +0800

    chore: simplify `Attach` trait and its implementations

--- error2/src/attach.rs
+++ error2/src/attach.rs
@@ -1,17 +1,16 @@
 use crate::{Error2, Location};
 
-pub trait Attach {
-    fn attach(self) -> Self;
-    fn attach_location(self, location: Location) -> Self;
-}
-
-impl<E: Error2> Attach for E {
+pub trait Attach: Sized {
     #[track_caller]
     #[inline]
     fn attach(self) -> Self {
         self.attach_location(Location::caller())
     }
 
+    fn attach_location(self, location: Location) -> Self;
+}
+
+impl<E: Error2> Attach for E {
     #[inline]
     fn attach_location(mut self, location: Location) -> Self {
         self.backtrace_mut().attach_location(location);
@@ -20,20 +19,11 @@ impl<E: Error2> Attach for E {
 }
 
 impl<T, E: Error2> Attach for Result<T, E> {
-    #[track_caller]
-    #[inline]
-    fn attach(self) -> Self {
-        self.attach_location(Location::caller())
-    }
-
     #[inline]
     fn attach_location(self, location: Location) -> Self {
         match self {
             Ok(t) => Ok(t),
-            Err(mut e) => {
-                e.backtrace_mut().attach_location(location);
-                Err(e)
-            }
+            Err(e) => Err(e.attach_location(location)),
         }
     }
 }
